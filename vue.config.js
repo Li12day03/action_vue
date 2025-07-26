a10fdc088e40b0cdc802dffaf37f89577c2577b6
李 <li12day@qq.com>
2025-07-26 17:56:18 +0800

    feat: init vue cli project

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require("@vue/cli-service");
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  // 1. 部署基础路径
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/action_vue/" // 仓库名
+      : "/",
+
+  // 2. 打包输出目录（默认 dist）
+  outputDir: "dist",
+});
